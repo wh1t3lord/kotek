33437392eb15cacb87099b75d62951345b2f99de
wh1t3lord <git.matvey@yandex.ru>
2022-02-23 15:08:30 +0300

    ADD: gl allocators

---
+++ engine/kotek.render.gl/kotek_render_static_buffer_pool_with_linear_allocator.cpp
@@ -0,0 +1,99 @@
+#include "kotek_render_static_buffer_pool_with_linear_allocator.h"
+
+namespace Kotek
+{
+	namespace Render
+	{
+		namespace gl
+		{
+			ktkRenderStaticBufferPool_LinearAllocator::
+				ktkRenderStaticBufferPool_LinearAllocator(void) :
+				m_is_need_to_recalculate{false},
+				m_memory_offset{}, m_total_memory{}, m_p_data{}
+			{
+			}
+
+			ktkRenderStaticBufferPool_LinearAllocator::
+				~ktkRenderStaticBufferPool_LinearAllocator(void)
+			{
+			}
+
+			void ktkRenderStaticBufferPool_LinearAllocator::Initialize(
+				ktk::uint32_t total_memory_size,
+				ktk::uint32_t count_of_entity_elements,
+				const char* vma_name_allocation)
+			{
+			}
+
+			void ktkRenderStaticBufferPool_LinearAllocator::Shutdown() {}
+
+			bool ktkRenderStaticBufferPool_LinearAllocator::AllocBuffer(
+				ktk::entity_t entity_id, ktk::uint32_t number_of_elements,
+				ktk::uint32_t stride_in_bytes, void** p_data) noexcept
+			{
+				return this->AllocBuffer(entity_id,
+					ktk::align_up<ktk::uint32_t>(
+						number_of_elements * stride_in_bytes, 256u),
+					p_data);
+			}
+
+			bool ktkRenderStaticBufferPool_LinearAllocator::Dealloc(
+				ktk::entity_t entity_id) noexcept
+			{
+				if (this->m_entities_offset.find(entity_id) ==
+					this->m_entities_offset.end())
+					return false;
+
+				KOTEK_ASSERT(
+					!(this->m_entities_vertex_size_with_stride.find(
+						  entity_id) ==
+						this->m_entities_vertex_size_with_stride.end()),
+					"can't be");
+
+				this->m_memory_offset = 0;
+				this->m_entities_offset.clear();
+
+				this->m_entities_vertex_size_with_stride.erase(entity_id);
+
+				this->m_is_need_to_recalculate = true;
+
+				return true;
+			}
+
+			bool ktkRenderStaticBufferPool_LinearAllocator::IsNeedToRecalculate(
+				void) const noexcept
+			{
+				return this->m_is_need_to_recalculate;
+			}
+
+			bool ktkRenderStaticBufferPool_LinearAllocator::AllocBuffer(
+				ktk::entity_t entity_id, ktk::uint32_t size,
+				void** p_data) noexcept
+			{
+				if (this->m_entities_offset.find(entity_id) !=
+					this->m_entities_offset.end())
+					return false;
+
+				KOTEK_ASSERT(
+					this->m_memory_offset + size < this->m_total_memory,
+					"Memory is overflowed. You need to increase size of pool "
+					"when initializing");
+
+				*p_data = reinterpret_cast<void*>(
+					this->m_p_data + this->m_memory_offset);
+
+				/* delete
+				                p_out->buffer = this->m_p_buffer;
+				                p_out->offset = this->m_memory_offset;
+				                p_out->range = size;*/
+
+				this->m_entities_offset[entity_id] = this->m_memory_offset;
+				this->m_entities_vertex_size_with_stride[entity_id] = size;
+
+				this->m_memory_offset += size;
+
+				return true;
+			}
+		} // namespace gl
+	}     // namespace Render
+} // namespace Kotek
---
+++ engine/kotek.render.gl/kotek_render_static_buffer_pool_with_linear_allocator.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "../kotek.core/kotek_std.h"
+
+namespace Kotek
+{
+	namespace Render
+	{
+		namespace gl
+		{
+			class ktkRenderStaticBufferPool_LinearAllocator
+			{
+			public:
+				ktkRenderStaticBufferPool_LinearAllocator(void);
+				~ktkRenderStaticBufferPool_LinearAllocator(void);
+
+				void Initialize(ktk::uint32_t total_memory_size,
+					ktk::uint32_t count_of_entity_elements,
+					const char* vma_name_allocation);
+
+				void Shutdown();
+
+				bool AllocBuffer(ktk::entity_t entity_id,
+					ktk::uint32_t number_of_elements,
+					ktk::uint32_t stride_in_bytes, void** p_data) noexcept;
+
+				bool Dealloc(ktk::entity_t entity_id) noexcept;
+
+				bool IsNeedToRecalculate(void) const noexcept;
+
+			private:
+				bool AllocBuffer(ktk::entity_t entity_id, ktk::uint32_t size,
+					void** p_data) noexcept;
+
+			private:
+				bool m_is_need_to_recalculate;
+				ktk::uint32_t m_memory_offset;
+				ktk::uint32_t m_total_memory;
+				char* m_p_data;
+				ktk::unordered_map<ktk::entity_t, ktk::uint32_t>
+					m_entities_offset;
+				ktk::unordered_map<ktk::entity_t, ktk::uint32_t>
+					m_entities_vertex_size_with_stride;
+			};
+		} // namespace gl
+	}     // namespace Render
+} // namespace Kotek
