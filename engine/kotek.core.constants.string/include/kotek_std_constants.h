403536abaee347d2c7f889ccb295b5aa7a87f4ee
wh1t3lord <git.matvey@yandex.ru>
2022-06-14 17:50:52 +0300

    ADD: new constants

--- engine/kotek.core.constants.string/include/kotek_std_constants.h
+++ engine/kotek.core.constants.string/include/kotek_std_constants.h
@@ -13,5 +13,25 @@ namespace Kotek
 #else
 		constexpr tchar kPathSeparator = KOTEK_TEXT('/');
 #endif
+
+		constexpr const tchar* kRenderer_OpenGL3_3_Name =
+			KOTEK_TEXT("OpenGL 3.3");
+		constexpr const tchar* kRenderer_OpenGL4_6_Name =
+			KOTEK_TEXT("OpenGL 4.6");
+
+		constexpr const tchar* kRenderer_DirectX_7_Name =
+			KOTEK_TEXT("DirectX 7");
+		constexpr const tchar* kRenderer_DirectX_8_Name =
+			KOTEK_TEXT("DirectX 8");
+		constexpr const tchar* kRenderer_DirectX_9_Name =
+			KOTEK_TEXT("DirectX 9");
+		constexpr const tchar* kRenderer_DirectX_10_Name =
+			KOTEK_TEXT("DirectX 10");
+		constexpr const tchar* kRenderer_DirectX_11_Name =
+			KOTEK_TEXT("DirectX 11");
+		constexpr const tchar* kRenderer_DirectX_12_Name =
+			KOTEK_TEXT("DirectX 12");
+
+		constexpr const tchar* kRenderer_Vulkan_Name = KOTEK_TEXT("Vulkan");
 	} // namespace ktk
 } // namespace Kotek
