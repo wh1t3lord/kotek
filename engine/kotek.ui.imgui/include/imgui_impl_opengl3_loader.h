bf41fb451aa8902039784dd060df947974eefee6
wh1t3lord <git.matvey@yandex.ru>
2022-08-17 19:17:55 +0300

    ADD: glad for imgui

--- engine/kotek.ui.imgui/include/imgui_impl_opengl3_loader.h
+++ engine/kotek.ui.imgui/include/imgui_impl_opengl3_loader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <kotek.core.defines.static.render.gl/include/kotek_core_defines_static_render_gl.h>
+
 #if (KOTEK_USE_OPENGL_MODERN_VERSION > 2)
 	#include <kotek.render.gl.glad3.3/include/kotek_render_gl_glad3.h>
 #endif
@@ -7,3 +9,8 @@
 #if (KOTEK_USE_OPENGL_MODERN_VERSION > 3)
 	#include <kotek.render.gl.glad4.6/include/kotek_render_gl_glad4.6.h>
 #endif
+
+int imgl3wInit(void)
+{
+	return 0;
+}
