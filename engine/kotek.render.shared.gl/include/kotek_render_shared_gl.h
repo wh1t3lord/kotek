055fd13512633384fa834454806a512bbb077fb6
wh1t3lord <git.matvey@yandex.ru>
2022-08-17 19:13:24 +0300

    ADD: returned, but changed

--- engine/kotek.render.shared.gl/include/kotek_render_shared_gl.h
+++ engine/kotek.render.shared.gl/include/kotek_render_shared_gl.h
@@ -4,14 +4,6 @@
 #include "kotek_render_graph_simplified_types_input.h"
 #include "kotek_render_graph_simplified_types_output.h"
 
-#if (KOTEK_USE_OPENGL_MODERN_VERSION > 2)
-#include <kotek.render.gl.glad3.3/include/kotek_render_gl_glad3.h>
-#endif
-
-#if (KOTEK_USE_OPENGL_MODERN_VERSION > 3)
-#include <kotek.render.gl.glad4.6/include/kotek_render_gl_glad4.6.h>
-#endif
-
 namespace Kotek
 {
 	namespace Core
