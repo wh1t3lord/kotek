8a270f001e15db3cc6f7e93a1ef94eec14bf3f88
wh1t3lord <git.matvey@yandex.ru>
2022-09-09 10:33:47 +0300

    ADD: handling custom types formatting project

---
+++ engine/kotek.core.format_custom_types/src/main_core_format_custom_types_dll.cpp
@@ -0,0 +1,27 @@
+#include "../include/kotek_core_format_custom_types.h"
+
+namespace Kotek
+{
+	namespace Core
+	{
+		bool InitializeModule_Core_Format_Custom_Types(ktkMainManager* p_manager)
+		{
+			return true;
+		}
+
+		bool SerializeModule_Core_Format_Custom_Types(ktkMainManager* p_manager)
+		{
+			return true;
+		}
+
+		bool DeserializeModule_Core_Format_Custom_Types(ktkMainManager* p_manager)
+		{
+			return true;
+		}
+
+		bool ShutdownModule_Core_Format_Custom_Types(ktkMainManager* p_manager)
+		{
+			return true;
+		}
+	} // namespace Core
+} // namespace Kotek
