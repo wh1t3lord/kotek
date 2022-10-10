f86c55ea3e3e17a1f40ccefe376c44cf7352422b
wh1t3lord <git.matvey@yandex.ru>
2022-10-10 16:58:33 +0300

    ADD: fnial

--- engine/kotek.core.resource_manager/include/kotek_core_resource_manager.h
+++ engine/kotek.core.resource_manager/include/kotek_core_resource_manager.h
@@ -4,13 +4,13 @@
 #include <kotek.core.resource_manager.saver/include/kotek_core_resource_manager_saver.h>
 #include "kotek_resource_manager.h"
 
-namespace Kotek
-{
-	namespace Core
-	{
-		class ktkMainManager;
-	}
-} // namespace Kotek
+KOTEK_BEGIN_NAMESPACE_KOTEK
+KOTEK_BEGIN_NAMESPACE_CORE
+
+class ktkMainManager;
+
+KOTEK_END_NAMESPACE_CORE
+KOTEK_END_NAMESPACE_KOTEK
 
 namespace Kotek
 {
@@ -20,5 +20,5 @@ namespace Kotek
 		bool SerializeModule_Core_Resource_Manager(ktkMainManager* p_manager);
 		bool DeserializeModule_Core_Resource_Manager(ktkMainManager* p_manager);
 		bool ShutdownModule_Core_Resource_Manager(ktkMainManager* p_manager);
-	}
+	} // namespace Core
 } // namespace Kotek
