aab3eefb3729e47a402115b9e1bce660f1e44318
wh1t3lord <git.matvey@yandex.ru>
2022-12-15 18:31:12 +0300

    ADD: eng section for documentation about cmake marcos

--- engine/kotek.exe/src/entry_point.cpp
+++ engine/kotek.exe/src/entry_point.cpp
@@ -1,6 +1,7 @@
 #include <kotek.core.main_manager/include/kotek_core_main_manager.h>
 #include <kotek.game/include/kotek_engine.h>
 
+// clang-format off
 /**
  * @mainpage Kotek
  *
@@ -89,6 +90,7 @@
  * Kotek::Engine::Shutdown_Engine(&main_manager);
  * @endcode
  */
+// clang-format on
 int main(int argc, char** argv)
 {
 	Kotek::Core::ktkMainManager main_manager(argc, argv);
@@ -99,14 +101,184 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-
+// clang-format off
 /**
  * \~english @page doxygen_user_guide_building_cmake_commands_description "Description of every flag (macroses) what you can pass"
  * 
+ * - @ref KOTEK_STD_LIBRARY
+ * - @ref KOTEK_UI_GAME_LIBRARY
+ * - @ref KOTEK_HIDE_CPP_DISABLE_ALL_WARNINGS_PREPROCESSOR
+ * - @ref KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR
+ * - @ref KOTEK_CPP_KEYWORD_NOEXCEPT
+ * - @ref KOTEK_CPP_KEYWORD_INLINE
+ * - @ref KOTEK_CPP_KEYWORD_CONSTEXPR
+ * - @ref KOTEK_MEMORY_LEAK_DETECTION
+ * - @ref KOTEK_CPP_MEMORY_LEAK_LIBRARY
+ * - @ref KOTEK_MEMORY_ALLOCATOR_CPU
+ * 
+ * \page KOTEK_STD_LIBRARY
+ * 
+ * This flag specifies what your library is. By our standard we support only two libraries it is STL from standard implementation that vendors provide like linux, Windows NT SDK and etc. And we support Boost library.
+ * 
+ * If argument is empty it means that by default CMake sets to "BOOST" variable and it means that your solution will be generated for Boost library if we don't want to generate solution for Boost library you need to pass STD otherwise if you pass any string that is not BOOST and not STD it means that you will include your own STL-like library.
+ * 
+ * @warning Engine doesn't support any kind of libraries. Engine supports only STL-like libraries that could override our containers.
+ * 
+ * Arguments: Empty string, STD, Boost, Any String.
+ * 
+ * Example if you want to generate for STD: 
+ * @code
+ *  cmake -DKOTEK_STD_LIBRARY=STD ..
+ * @endcode
+ * 
+ * \page KOTEK_UI_GAME_LIBRARY
+ * 
+ * This flag specifies what your UI library is. By our standard we support the following libraries:
+ * 
+ * 1. RmlUi
+ * 2. Chromium embedded framework (CEF) 
+ * 
+ * @warning Current state of CEF is that we can't implement because of package managers for C++ that don't support it well now. When it was real that some of C++ package managers will support CEF compilation (or at least good installation process) we will add, but it is only a plan to support this library for UI.
+ * 
+ * By default it will set to RMLUI value and it means that engine uses RMLUI library.
+ * 
+ * Arguments: Empty string, RMLUI, CEF (not supported now).
+ * 
+ * Example if I want to use CEF (if it would exist) instead of default RMLUI
+ * @code
+ *	cmake -DKOTEK_UI_GAME_LIBRARY=CEF ..
+ * @endcode
+ * 
+ * @note This flag generates two new macroses KOTEK_UI_GAME_LIBRARY_NAME and KOTEK_UI_GAME_LIBRARY_DESCRIPTION_NAME. KOTEK_UI_GAME_LIBRARY_NAME is formal naming of library and it is a string (char array) and KOTEK_UI_GAME_LIBRARY_DESCRIPTION_NAME stands for detailed description. But currently they just exist and you can use them as you want in your solution. 
+ * 
+ * \page KOTEK_HIDE_CPP_DISABLE_ALL_WARNINGS_PREPROCESSOR
+ * 
+ * This flag translates as: "Disable all warnings". So it accepts two states you enable a such feature or don't use it.
+ * 
+ * By default this flag is not used. In order to enable it you need to pass ON word
+ * 
+ * Example if I want to enable this feature
+ * @code
+ *	cmake -DKOTEK_HIDE_CPP_DISABLE_ALL_WARNINGS_PREPROCESSOR=ON ..
+ * @endcode
+ * 
+ * @note When you enabled this feature it means that you can write KOTEK_CPP_DISABLE_ALL_WARNINGS_PUSH and KOTEK_CPP_DISABLE_ALL_WARNINGS_POP. It tries to handle all compiler specific pragmas in order to disable all possible warnings. Sometimes it could be really useful if you have a strict compilation process and for some part of solution that you don't handle at all you want to disable warnings, but you want to keep compilation of your code with treating on warnings (it means it will stop compilation process if compiler finds some compiling warnings).
+ * 
+ * \page KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR
+ * 
+ * This flag stands for implement preprocessor for "hiding" language keywords like noexcept, constexpr, etc in order to enable them or disable them or just replace them in code on your specified keyword (@see KOTEK_CPP_KEYWORD_CONSTEXPR, @see KOTEK_CPP_KEYWORD_INLINE, @see KOTEK_CPP_KEYWORD_NOEXCEPT and others).
+ * 
+ * So by default CMake doesn't use it, but in order to enable it you need to pass ON word.
+ * 
+ * Example if I want to enable this feature:
+ * @code
+ *	cmake -DKOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR=ON ..
+ * @endcode
+ * 
+ * \page KOTEK_CPP_KEYWORD_NOEXCEPT
+ * 
+ * @warning this keyword depends on @see KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR if this macro is diabled the chaning of KOTEK_CPP_KEYWORD_NOEXCEPT doesn't make any sense and all changes will not be generated for solution.
+ * 
+ * This flag reprensents preprocessor for using in solution in order to replace native noexcept keyword.
+ * 
+ * You can specify any string 
+ * 
+ * By default sets to noexcept and it doesn't depend specified you KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR or not. 
+ * 
+ * Example if I want to change this macro (but you need to be sure that it will not cause compilation error)
+ * 
+ * @code 
+ *	cmake -DKOTEK_CPP_KEYWORD_NOEXCEPT=something ..
+ * @endcode
+ * 
+ * But if you want to generate solution without any noexcept keyword that we used you just need to pass empty string like this:
+ * 
+ * @code 
+ *	cmake -DKOTEK_CPP_KEYWORD_NOEXCEPT="" ..
+ * @endcode
+ * 
+ * \page KOTEK_CPP_KEYWORD_INLINE
+ * 
+ * @warning this keyword depends on @see KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR if this macro is diabled the chaning of KOTEK_CPP_KEYWORD_INLINE doesn't make any sense and all changes will not be generated for solution
+ * 
+ * This flag represents preprocessor for using in solution in order to replace native inline keyword.
+ * 
+ * You can specify any string 
+ * 
+ * By default sets to inline and it doesn't depend specified you KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR or not.
+ * 
+ * Example if I want to change this macro (but you need to be sure that it will not cause compilation error)
+ * 
+ * @code
+ *	cmake -DKOTEK_CPP_KEYWORD_INLINE="something" ..
+ * @endcode
+ * 
+ * But if you want to generate solution without any inline keyword that we used you just need to pass empty string like this:
+ * 
+ * @code
+ *	cmake -DKOTEK_CPP_KEYWORD_INLINE="" ..
+ * @endcode
+ * 
+ * \page KOTEK_CPP_KEYWORD_CONSTEXPR
+ * 
+ * This flag represents preprocessor for using in solution in order to replace native constexpr keyword.
+ * 
+ * You can specify any string
+ * 
+ * By default sets to constexpr and it doesn't depend specified you KOTEK_HIDE_CPP_KEYWORD_PREPROCESSOR or not.
+ * 
+ * Example if I want to change this macro (but you need to be sure that it will not cause compilation error)
+ * 
+ * @code
+ *	cmake -DKOTEK_CPP_KEYWORD_CONSTEXPR="something" ..
+ * @endcode
+ * 
+ * But if you want to generate solution without any constexpr keyword that we used you just need to pass empty string like this:
+ * 
+ * @code
+ *	cmake -DKOTEK_CPP_KEYWORD_CONSTEXPR="" ..
+ * @endcode
+ * 
+ * \page KOTEK_MEMORY_LEAK_DETECTION
+ * 
+ * This flag represents do we enable memory leak detection or not.
+ * 
+ * @warning this flag depends on CMAKE_BUILD_TYPE value if it is Debug it means that by default sets to ON. So we enable this feature
+ * 
+ * Arguments: Empty string, ON, Any string.
+ * 
+ * If you pass a string that is not empty and it is not equal to ON it means that this feature will be disabled.
+ * 
+ * But you need to use it when CMAKE_BUILD_TYPE=Debug.
+ * 
+ * Example if I don't want to use memory leak detection when build type is Debug.
+ * 
+ * @code
+ *	cmake -DCMAKE_BUILD_TYPE=Debug -DKOTEK_MEMORY_LEAK_DETECTION=OFF ..
+ * @endcode
+ * 
+ * \page KOTEK_CPP_MEMORY_LEAK_LIBRARY
+ * 
+ * This library represents what library will be used in solution. 
+ * 
+ * By default we support Visual Leak Detector on Windows. 
+ * 
+ * @warning others platforms are not supported, except Windows NT
+ * 
+ * @warning This macro depends on KOTEK_MEMORY_LEAK_DETECTION value. If this feature is disabled it means that KOTEK_CPP_MEMORY_LEAK_LIBRARY is not affect on solution.
+ * 
+ * So by default sets to VLD value. It means that solution will use Visual Leak Detector library if it is presented in dependency folder.
+ * 
+ * Example if I want to change to other library:
+ * 
+ * @code
+ *	cmake -DCMAKE_BUILD_TYPE=Debug -DKOTEK_CPP_MEMORY_LEAK_LIBRARY="XXX" ..
+ * @endcode
+ * 
+ * \page KOTEK_MEMORY_ALLOCATOR_CPU
+ * 
+ * 
+ * 
  * \~russian @page doxygen_user_guide_building_cmake_commands_description 
  */
-
-
-
-
-
+// clang-format on
