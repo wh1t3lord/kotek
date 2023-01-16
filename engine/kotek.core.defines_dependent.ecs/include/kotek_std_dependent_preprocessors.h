d46218f16b0564265765a5c01eced75da71364d7
wh1t3lord <git.matvey@yandex.ru>
2023-01-16 21:36:04 +0300

    ADD: some design

--- engine/kotek.core.defines_dependent.ecs/include/kotek_std_dependent_preprocessors.h
+++ engine/kotek.core.defines_dependent.ecs/include/kotek_std_dependent_preprocessors.h
@@ -1,25 +1,34 @@
 #pragma once
 
-#define KOTEK_COMPONENT(YourClass)                                   \
-public:                                                              \
-	static const Kotek::ktk::string& GetComponentName(void) noexcept \
-	{                                                                \
-		return m_component_name;                                     \
-	}                                                                \
-	static Kotek::ktk::size_t GetHashCode(void) noexcept             \
-	{                                                                \
-		return typeid(YourClass).hash_code();                        \
-	}                                                                \
-	void SetEnabled(bool status) noexcept                            \
-	{                                                                \
-		this->m_is_enabled = status;                                 \
-	}                                                                \
-                                                                     \
-	bool IsEnabled(void) const noexcept                              \
-	{                                                                \
-		return this->m_is_enabled;                                   \
-	}                                                                \
-                                                                     \
-private:                                                             \
-	inline static Kotek::ktk::string m_component_name = #YourClass;  \
+#define KOTEK_COMPONENT(YourClass)                                          \
+public:                                                                     \
+	static const Kotek::ktk::string& GetComponentName(void) noexcept        \
+	{                                                                       \
+		return m_component_name;                                            \
+	}                                                                       \
+	static Kotek::ktk::size_t GetHashCode(void) noexcept                    \
+	{                                                                       \
+		return typeid(YourClass).hash_code();                               \
+	}                                                                       \
+	void SetEnabled(bool status) noexcept                                   \
+	{                                                                       \
+		this->m_is_enabled = status;                                        \
+	}                                                                       \
+                                                                            \
+	bool IsEnabled(void) const noexcept                                     \
+	{                                                                       \
+		return this->m_is_enabled;                                          \
+	}                                                                       \
+                                                                            \
+	Kotek::ktk::json::value Serialize(void) noexcept override               \
+	{                                                                       \
+		return Kotek::ktk::json::value_from(*this);                         \
+	}                                                                       \
+	void Deserialize(const Kotek::ktk::json::value& data) noexcept override \
+	{                                                                       \
+		*this = Kotek::ktk::json::value_to<YourClass>(data);                \
+	}                                                                       \
+                                                                            \
+private:                                                                    \
+	inline static Kotek::ktk::string m_component_name = #YourClass;         \
 	bool m_is_enabled = true;
