d145ad6d1a2f99f24576b393e1eb771bbdc15797
wh1t3lord <git.matvey@yandex.ru>
2023-01-22 18:50:49 +0300

    ADD: rmlui

---
+++ engine/kotek.ui.rmlui/include/kotek_ui_rmlui.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <kotek.ui.imgui/include/kotek_ui_imgui.h>
+
+#ifdef KOTEK_USE_CEF_LIBRARY
+	#include <kotek.ui.cef/include/kotek_ui_cef.h>
+#endif
+
+KOTEK_BEGIN_NAMESPACE_KOTEK
+KOTEK_BEGIN_NAMESPACE_CORE
+
+class ktkMainManager;
+
+KOTEK_END_NAMESPACE_CORE
+KOTEK_END_NAMESPACE_KOTEK
+
+KOTEK_BEGIN_NAMESPACE_KOTEK
+KOTEK_BEGIN_NAMESPACE_UI
+
+bool InitializeModule_UI(Core::ktkMainManager* p_main_manager);
+bool ShutdownModule_UI(Core::ktkMainManager* p_main_manager);
+bool SerializeModule_UI(Core::ktkMainManager* p_main_manager);
+bool DeserializeModule_UI(Core::ktkMainManager* p_main_manager);
+
+KOTEK_END_NAMESPACE_UI
+KOTEK_END_NAMESPACE_KOTEK
