662de11ff1acb7505fc5998716263b8b47570648
wh1t3lord <git.matvey@yandex.ru>
2023-03-21 15:41:01 +0300

    ADD: continue

--- engine/kotek.render.shared.gl/include/kotek_render_data_types.h
+++ engine/kotek.render.shared.gl/include/kotek_render_data_types.h
@@ -34,6 +34,62 @@ namespace gl
 		/// @brief enum for determining what is the type of shader
 		gl::eShaderType m_shader_type;
 	};
+
+	// for indirect rendering
+	class ktkDrawVertexIndirectCommand
+	{
+	public:
+		ktkDrawVertexIndirectCommand();
+		~ktkDrawVertexIndirectCommand();
+
+		GLuint Get_Count(void) const noexcept;
+		void Set_Count(GLuint value) noexcept;
+
+		GLuint Get_InstanceCount(void) const noexcept;
+		void Set_InstanceCount(GLuint value) noexcept;
+
+		GLuint Get_First(void) const noexcept;
+		void Set_First(GLuint value) noexcept;
+
+		GLuint Get_Reserved(void) const noexcept;
+		void Set_Reserved(GLuint value) noexcept;
+
+	private:
+		GLuint m_count;
+		GLuint m_instance_count;
+		GLuint m_first;
+		GLuint m_reserved;
+	};
+
+	// for indirect rendering
+	class ktkDrawIndexIndirectCommand
+	{
+	public:
+		ktkDrawIndexIndirectCommand();
+		~ktkDrawIndexIndirectCommand();
+
+		GLuint Get_Count(void) const noexcept;
+		void Set_Count(GLuint value) noexcept;
+
+		GLuint Get_InstanceCount(void) const noexcept;
+		void Set_InstanceCount(GLuint value) noexcept;
+
+		GLuint Get_FirstIndex(void) const noexcept;
+		void Set_FirstIndex(GLuint value) noexcept;
+
+		GLuint Get_BaseVertex(void) const noexcept;
+		void Set_BaseVertex(GLuint value) noexcept;
+
+		GLuint Get_ReservedMustBeZero(void) const noexcept;
+		void Set_ReservedMustBeZero(GLuint value) noexcept;
+
+	private:
+		GLuint m_count;
+		GLuint m_instance_count;
+		GLuint m_first_index;
+		GLuint m_base_vertex;
+		GLuint m_reserved_must_be_zero;
+	};
 } // namespace gl
 
 KOTEK_END_NAMESPACE_RENDER
