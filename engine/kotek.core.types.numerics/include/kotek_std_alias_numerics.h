eb70a5641c490934481aa6ada2eafa01422459ac
wh1t3lord <git.matvey@yandex.ru>
2024-10-12 00:12:34 +0300

    ADD: making user life better

--- engine/kotek.core.types.numerics/include/kotek_std_alias_numerics.h
+++ engine/kotek.core.types.numerics/include/kotek_std_alias_numerics.h
@@ -25,4 +25,22 @@ using double_t = double;
 using long_double_t = long double;
 
 KOTEK_END_NAMESPACE_KTK
+
+using uint64_t = KUN_KOTEK KUN_KTK uint64_t;
+using uint32_t = KUN_KOTEK KUN_KTK uint32_t;
+using uint16_t = KUN_KOTEK KUN_KTK uint16_t;
+using uint8_t = KUN_KOTEK KUN_KTK uint8_t;
+using int64_t = KUN_KOTEK KUN_KTK int64_t;
+using int32_t = KUN_KOTEK KUN_KTK int32_t;
+using int16_t = KUN_KOTEK KUN_KTK int16_t;
+using int8_t = KUN_KOTEK KUN_KTK int8_t;
+using size_t = KUN_KOTEK KUN_KTK size_t;
+using ptrdiff_t = KUN_KOTEK KUN_KTK ptrdiff_t;
+using enum_base_t = KUN_KOTEK KUN_KTK enum_base_t;
+
+using float_t = KUN_KOTEK KUN_KTK float_t;
+using double_t = KUN_KOTEK KUN_KTK double_t;
+using long_double_t = KUN_KOTEK KUN_KTK long_double_t;
+
+
 KOTEK_END_NAMESPACE_KOTEK
