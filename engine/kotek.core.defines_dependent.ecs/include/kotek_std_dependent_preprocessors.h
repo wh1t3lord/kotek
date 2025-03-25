6223e40b24ed37ec3bcdbd3d39085bcf832d5d73
wh1t3lord <git.matvey@yandex.ru>
2025-03-25 23:24:39 +0300

    ADD: refactor for being universal removed hardcode;

--- engine/kotek.core.defines_dependent.ecs/include/kotek_std_dependent_preprocessors.h
+++ engine/kotek.core.defines_dependent.ecs/include/kotek_std_dependent_preprocessors.h
@@ -1,58 +1,23 @@
 #pragma once
 
-#define KOTEK_COMPONENT(YourClass)                                            \
-public:                                                                       \
-	static const kotek::static_cstring_t<zircon_DEF_MAX_COMPONENT_NAME_SIZE>& \
-	GetComponentName(void) noexcept                                           \
-	{                                                                         \
-		return m_component_name;                                              \
-	}                                                                         \
-	static kotek::size_t GetComponentNameHash(void) noexcept                  \
-	{                                                                         \
-		return m_component_name_hash;                                         \
-	}                                                                         \
-	static kotek::size_t GetHashCode(void) noexcept                           \
-	{                                                                         \
-		return typeid(YourClass).hash_code();                                 \
-	}                                                                         \
-	void SetEnabled(bool status) noexcept                                     \
-	{                                                                         \
-		this->m_is_enabled = status;                                          \
-	}                                                                         \
-                                                                              \
-	bool IsEnabled(void) const noexcept                                       \
-	{                                                                         \
-		return this->m_is_enabled;                                            \
-	}                                                                         \
-                                                                              \
-	kotek::ktk::json::value Serialize(void) noexcept override                 \
-	{                                                                         \
-		return kotek::ktk::json::value_from(*this);                           \
-	}                                                                         \
-	void Deserialize(const Kotek::ktk::json::value& data) noexcept override   \
-	{                                                                         \
-		*this = Kotek::ktk::json::value_to<YourClass>(data);                  \
-	}                                                                         \
-                                                                              \
-	kotek::ktk::json::value Serialize(                                        \
-		unsigned char* p_raw_memory, Kotek::ktk::size_t size) override        \
-	{                                                                         \
-		KOTEK_ASSERT(p_raw_memory, "you passed an invalid part of memory!");  \
-		kotek::ktk::json::static_resource res(p_raw_memory, size);            \
-		kotek::ktk::json::storage_ptr ptr(&res);                              \
-		return Kotek::ktk::json::value_from(*this, ptr);                      \
-	}                                                                         \
-                                                                              \
-	kotek::uint8_t get_component_type(void) const noexcept override           \
-	{                                                                         \
-		return m_component_type;                                              \
-	}                                                                         \
-                                                                              \
-private:                                                                      \
-	inline static kotek::static_cstring_t<zircon_DEF_MAX_COMPONENT_NAME_SIZE> \
-		m_component_name = #YourClass;                                        \
-	inline static kotek::size_t m_component_name_hash = kotek::hash_t<        \
-		kotek::static_cstring_t<zircon_DEF_MAX_COMPONENT_NAME_SIZE>>{}(       \
-		m_component_name);                                                    \
-	kotek::uint8_t m_component_type = kComponentType##YourClass;              \
-	bool m_is_enabled = true;
+// add here only static methods and fields!!!
+// because field generator will fail
+#define KOTEK_COMPONENT(YourClass, StringType)               \
+public:                                                      \
+	static const StringType& GetComponentName(void) noexcept \
+	{                                                        \
+		return m_component_name;                             \
+	}                                                        \
+	static kotek::size_t GetComponentNameHash(void) noexcept \
+	{                                                        \
+		return m_component_name_hash;                        \
+	}                                                        \
+	static kotek::size_t GetHashCode(void) noexcept          \
+	{                                                        \
+		return typeid(YourClass).hash_code();                \
+	}                                                        \
+                                                             \
+private:                                                     \
+	inline static StringType m_component_name = #YourClass;  \
+	inline static kotek::size_t m_component_name_hash =      \
+		kotek::hash_t<StringType>{}(m_component_name);
