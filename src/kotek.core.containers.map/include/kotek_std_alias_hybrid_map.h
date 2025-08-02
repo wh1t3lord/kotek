728f0eccdd2fa202b14337b2ba3db45a02f0daa1
wh1t3lord <git.matvey@yandex.ru>
2025-08-02 12:21:57 +0300

    ADD: compilation fixes of vector;

---
+++ src/kotek.core.containers.map/include/kotek_std_alias_hybrid_map.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <kotek.core.defines.static.cpp/include/kotek_core_defines_static_cpp.h>
+#include <kotek.core.memory.cpu/include/kotek_core_memory_cpu.h>
+
+KOTEK_BEGIN_NAMESPACE_KOTEK
+KOTEK_BEGIN_NAMESPACE_KTK
+
+
+
+KOTEK_END_NAMESPACE_KTK
+KOTEK_END_NAMESPACE_KOTEK
--- src/kotek.core.containers.vector/include/kotek_std_alias_hybrid_vector.h
+++ src/kotek.core.containers.vector/include/kotek_std_alias_hybrid_vector.h
@@ -97,7 +97,7 @@ public:
 	}
 
 	template <class InputIt>
-	hybrid_vector_impl(InputIt first, InputIt last) : mem<InputIt>(first, last)
+	hybrid_vector_impl(InputIt first, InputIt last) : mem(first, last)
 	{
 	}
 
@@ -107,212 +107,184 @@ public:
 			__hv_container_namespace::is_same_v<Type, Type2>>>
 	hybrid_vector_impl(
 		const hybrid_vector_impl<Type2, ElementCount2, Realloc2>& other) :
-		mem<Type2, ElementCount2, Realloc2>(other)
+		mem(other)
 	{
 	}
 
-	hybrid_vector_impl(const hybrid_vector_impl& other) : mem(other)
-	{
-	}
+	hybrid_vector_impl(const hybrid_vector_impl& other) : mem(other) {}
 
 	template <typename Type2, kun_ktk size_t ElementCount2, bool Realloc2,
 		typename = __hv_container_namespace::enable_if_t<
 			(ElementCount >= ElementCount2 || Realloc == true) &&
 			__hv_container_namespace::is_same_v<Type, Type2>>>
 	hybrid_vector_impl(
-		hybrid_vector_impl<Type2, ElementCount2, Realloc2>&& other) :
-		pool{memory, __hv_Size,
-			Realloc ? __hv_container_namespace_pmr::get_default_resource()
-					: __hv_container_namespace_pmr::null_memory_resource()},
-		con{__hv_container_namespace::move(other.container_move()), &pool}
+		hybrid_vector_impl<Type2, ElementCount2, Realloc2>&& other) : mem(other)
 	{
-		if constexpr (ElementCount > 0)
-		{
-			con.reserve(ElementCount);
-		}
 	}
 
-	hybrid_vector_impl(hybrid_vector_impl&& other) noexcept :
-		pool{(ElementCount == 0) ? nullptr : buf,
-			(ElementCount == 0) ? 0 : __hv_Size,
-			Realloc ? __hv_container_namespace_pmr::get_default_resource()
-					: __hv_container_namespace_pmr::null_memory_resource()},
-		con{__hv_container_namespace::move(other.vec), &pool}
-	{
-		if constexpr (ElementCount > 0)
-		{
-			con.reserve(ElementCount);
-		}
-	}
+	hybrid_vector_impl(hybrid_vector_impl&& other) noexcept : mem(other) {}
 
-	hybrid_vector_impl(__hv_il_t<Type> init) :
-		pool{(ElementCount == 0) ? nullptr : buf,
-			(ElementCount == 0) ? 0 : __hv_Size,
-			Realloc ? __hv_container_namespace_pmr::get_default_resource()
-					: __hv_container_namespace_pmr::null_memory_resource()},
-		con{init, &pool}
-	{
-		if constexpr (ElementCount > 0)
-		{
-			con.reserve(ElementCount);
-		}
-	}
+	hybrid_vector_impl(__hv_il_t<Type> init) : mem(init) {}
 
 	~hybrid_vector_impl() {}
 
 public:
-	reference at(size_type pos) { return con.at(pos); }
-	const_reference at(size_type pos) const { return con.at(pos); }
-	reference operator[](size_type pos) { return con[pos]; }
-	const_reference operator[](size_type pos) const { return con[pos]; }
-	reference front() { return con.front(); }
-	const_reference front() const { return con.front(); }
-	reference back() { return con.back(); }
-	const_reference back() const { return con.back(); }
-	pointer data() noexcept { return con.data(); }
-	const_pointer data() const noexcept { return con.data(); }
+	reference at(size_type pos) { return mem.con.at(pos); }
+	const_reference at(size_type pos) const { return mem.con.at(pos); }
+	reference operator[](size_type pos) { return mem.con[pos]; }
+	const_reference operator[](size_type pos) const { return mem.con[pos]; }
+	reference front() { return mem.con.front(); }
+	const_reference front() const { return mem.con.front(); }
+	reference back() { return mem.con.back(); }
+	const_reference back() const { return mem.con.back(); }
+	pointer data() noexcept { return mem.con.data(); }
+	const_pointer data() const noexcept { return mem.con.data(); }
 
-	iterator begin() noexcept { return con.begin(); }
-	const_iterator begin() const noexcept { return con.begin(); }
-	const_iterator cbegin() const noexcept { return con.cbegin(); }
+	iterator begin() noexcept { return mem.con.begin(); }
+	const_iterator begin() const noexcept { return mem.con.begin(); }
+	const_iterator cbegin() const noexcept { return mem.con.cbegin(); }
 
-	iterator end() noexcept { return con.end(); }
-	const_iterator end() const noexcept { return con.end(); }
-	const_iterator cend() const noexcept { return con.cend(); }
+	iterator end() noexcept { return mem.con.end(); }
+	const_iterator end() const noexcept { return mem.con.end(); }
+	const_iterator cend() const noexcept { return mem.con.cend(); }
 
-	reverse_iterator rbegin() noexcept { return con.rbegin(); }
-	const_reverse_iterator rbegin() const noexcept { return con.rbegin(); }
-	const_reverse_iterator crbegin() const noexcept { return con.crbegin(); }
+	reverse_iterator rbegin() noexcept { return mem.con.rbegin(); }
+	const_reverse_iterator rbegin() const noexcept { return mem.con.rbegin(); }
+	const_reverse_iterator crbegin() const noexcept
+	{
+		return mem.con.crbegin();
+	}
 
-	reverse_iterator rend() noexcept { return con.rend(); }
-	const_reverse_iterator rend() const noexcept { return con.rend(); }
-	const_reverse_iterator crend() const noexcept { return con.crend(); }
+	reverse_iterator rend() noexcept { return mem.con.rend(); }
+	const_reverse_iterator rend() const noexcept { return mem.con.rend(); }
+	const_reverse_iterator crend() const noexcept { return mem.con.crend(); }
 
-	bool empty() const noexcept { return con.empty(); }
-	size_type size() const noexcept { return con.size(); }
-	size_type max_size() const noexcept { return con.max_size(); }
-	void reserve(size_type new_cap) { con.reserve(new_cap); }
-	size_type capacity() const noexcept { return con.capacity(); }
-	void shrink_to_fit() { con.shrink_to_fit(); }
+	bool empty() const noexcept { return mem.con.empty(); }
+	size_type size() const noexcept { return mem.con.size(); }
+	size_type max_size() const noexcept { return mem.con.max_size(); }
+	void reserve(size_type new_cap) { mem.con.reserve(new_cap); }
+	size_type capacity() const noexcept { return mem.con.capacity(); }
+	void shrink_to_fit() { mem.con.shrink_to_fit(); }
 
-	void clear() noexcept { con.clear(); }
+	void clear() noexcept { mem.con.clear(); }
 
 	template <typename... Args>
 	reference emplace_back(Args&&... args)
 	{
-		return con.emplace_back(
+		return mem.con.emplace_back(
 			__hv_container_namespace::forward<Args>(args)...);
 	}
 
 	template <class... Args>
 	iterator emplace(const_iterator pos, Args&&... args)
 	{
-		return con.emplace(__hv_container_namespace::forward<Args>(args)...);
+		return mem.con.emplace(
+			__hv_container_namespace::forward<Args>(args)...);
 	}
 
 	iterator insert(const_iterator pos, const Type& value)
 	{
-		return con.insert(pos, value);
+		return mem.con.insert(pos, value);
 	}
 	iterator insert(const_iterator pos, Type&& value)
 	{
-		return con.insert(pos, __hv_container_namespace::move(value));
+		return mem.con.insert(pos, __hv_container_namespace::move(value));
 	}
 	iterator insert(const_iterator pos, size_type count, const Type& value)
 	{
-		return con.insert(pos, count, value);
+		return mem.con.insert(pos, count, value);
 	}
 
 	template <class InputIt>
 	iterator insert(const_iterator pos, InputIt first, InputIt last)
 	{
-		return con.insert<InputIt>(pos, first, last);
+		return mem.con.insert<InputIt>(pos, first, last);
 	}
 
 	iterator insert(const_iterator pos, __hv_il_t<Type> ilist)
 	{
-		return con.insert(pos, ilist);
+		return mem.con.insert(pos, ilist);
 	}
 
-	void push_back(const Type& value) { con.push_back(value); }
+	void push_back(const Type& value) { mem.con.push_back(value); }
 	void push_back(Type&& value)
 	{
-		con.push_back(__hv_container_namespace::move(value));
+		mem.con.push_back(__hv_container_namespace::move(value));
 	}
-	void pop_back() { con.pop_back(); }
+	void pop_back() { mem.con.pop_back(); }
 
-	void resize(size_type count) { con.resize(count); }
+	void resize(size_type count) { mem.con.resize(count); }
 	void resize(size_type count, const Type& value)
 	{
-		con.resize(count, value);
+		mem.con.resize(count, value);
 	}
 
 	void swap(hybrid_vector_impl& other) noexcept
 	{
-		__hv_container_namespace_swap::swap(con, other.con);
+		__hv_container_namespace_swap::swap(mem.con, other.mem.con);
 	}
 
-	iterator erase(iterator pos) { return con.erase(pos); }
-	iterator erase(const_iterator pos) { return con.erase(pos); }
+	iterator erase(iterator pos) { return mem.con.erase(pos); }
+	iterator erase(const_iterator pos) { return mem.con.erase(pos); }
 	iterator erase(iterator first, iterator last)
 	{
-		return con.erase(first, last);
+		return mem.con.erase(first, last);
 	}
 	iterator erase(const_iterator first, const_iterator last)
 	{
-		return con.erase(first, last);
+		return mem.con.erase(first, last);
 	}
 
 	void assign(size_type count, const Type& value)
 	{
-		return con.assign(count, value);
+		return mem.con.assign(count, value);
 	}
 
 	template <class InputIt>
 	void assign(InputIt first, InputIt last)
 	{
-		return con.assign<InputIt>(first, last);
+		return mem.con.assign<InputIt>(first, last);
 	}
 
-	void assign(__hv_il_t<Type> ilist) { return con.assign(ilist); }
+	void assign(__hv_il_t<Type> ilist) { return mem.con.assign(ilist); }
 
-	allocator_type get_allocator() const { return con.get_allocator(); }
+	allocator_type get_allocator() const { return mem.con.get_allocator(); }
 
 	hybrid_vector_impl& operator=(const hybrid_vector_impl& other)
 	{
-		con.operator=(other.con);
+		mem.con.operator=(other.con);
 		return *this;
 	}
 	hybrid_vector_impl& operator=(const container_type& other)
 	{
-		con.operator=(other);
+		mem.con.operator=(other);
 		return *this;
 	}
 
 	hybrid_vector_impl& operator=(hybrid_vector_impl&& other)
 	{
-		con.operator=(__hv_container_namespace::move(other.con));
+		mem.con.operator=(__hv_container_namespace::move(other.con));
 		return *this;
 	}
 
 	hybrid_vector_impl& operator=(container_type&& other)
 	{
-		con.operator=(__hv_container_namespace::move(other));
+		mem.con.operator=(__hv_container_namespace::move(other));
 		return *this;
 	}
 
 	hybrid_vector_impl& operator=(__hv_il_t<value_type> ilist)
 	{
-		con.operator=(ilist);
+		mem.con.operator=(ilist);
 		return *this;
 	}
 
 public:
-	const container_type& container(void) const noexcept { return con; }
-	container_type& container(void) noexcept { return con; }
+	const container_type& container(void) const noexcept { return mem.con; }
+	container_type& container(void) noexcept { return mem.con; }
 	container_type&& container_move(void) noexcept
 	{
-		return __hv_container_namespace::move(con);
+		return __hv_container_namespace::move(mem.con);
 	}
 
 	constexpr kun_ktk size_t size_preallocated(void) const noexcept
@@ -402,17 +374,53 @@ private:
 			}
 		}
 
-		layout_prealloc_t(const hybrid_vector_impl& other) pool{
-			(ElementCount == 0) ? nullptr : buf,
-			(ElementCount == 0) ? 0 : __hv_Size,
-			Realloc ? __hv_container_namespace_pmr::get_default_resource()
-					: __hv_container_namespace_pmr::null_memory_resource()},
+		layout_prealloc_t(
+			const hybrid_vector_impl<Type, ElementCount, Realloc, __hv_Size>&
+				other) :
+			pool{(ElementCount == 0) ? nullptr : buf,
+				(ElementCount == 0) ? 0 : __hv_Size,
+				Realloc ? __hv_container_namespace_pmr::get_default_resource()
+						: __hv_container_namespace_pmr::null_memory_resource()},
 			con{other.container(), &pool}
 		{
 			if constexpr (ElementCount > 0)
 			{
 				con.reserve(ElementCount);
 			}
+		}
+
+		template <typename Type2, kun_ktk size_t ElementCount2, bool Realloc2,
+			typename = __hv_container_namespace::enable_if_t<
+				(ElementCount >= ElementCount2 || Realloc == true) &&
+				__hv_container_namespace::is_same_v<Type, Type2>>>
+		layout_prealloc_t(
+			hybrid_vector_impl<Type2, ElementCount2, Realloc2>&& other) :
+			pool{(ElementCount == 0) ? nullptr : buf,
+				(ElementCount == 0) ? 0 : __hv_Size,
+				Realloc ? __hv_container_namespace_pmr::get_default_resource()
+						: __hv_container_namespace_pmr::null_memory_resource()},
+			con{__hv_container_namespace::move(other.container_move()), &pool}
+		{
+		}
+
+		layout_prealloc_t(
+			hybrid_vector_impl<Type, ElementCount, Realloc, __hv_Size>&&
+				other) noexcept :
+			pool{(ElementCount == 0) ? nullptr : buf,
+				(ElementCount == 0) ? 0 : __hv_Size,
+				Realloc ? __hv_container_namespace_pmr::get_default_resource()
+						: __hv_container_namespace_pmr::null_memory_resource()},
+			con{__hv_container_namespace::move(other.container_move()), &pool}
+		{
+		}
+
+		layout_prealloc_t(__hv_il_t<Type> init) :
+			pool{(ElementCount == 0) ? nullptr : buf,
+				(ElementCount == 0) ? 0 : __hv_Size,
+				Realloc ? __hv_container_namespace_pmr::get_default_resource()
+						: __hv_container_namespace_pmr::null_memory_resource()},
+			con{init, &pool}
+		{
 		}
 
 		unsigned char buf[__hv_Size == 0 ? 1 : __hv_Size];
