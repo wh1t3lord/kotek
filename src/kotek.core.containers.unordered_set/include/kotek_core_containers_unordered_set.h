946ab48ca140dc34c3a19acca923e7b15fc0957e
wh1t3lord <git.matvey@yandex.ru>
2025-08-02 12:34:47 +0300

    ADD: refactor of unordered_set;

--- src/kotek.core.containers.unordered_set/include/kotek_core_containers_unordered_set.h
+++ src/kotek.core.containers.unordered_set/include/kotek_core_containers_unordered_set.h
@@ -1,6 +1,55 @@
 #pragma once
 
 #include "kotek_std_alias_unordered_set.h"
+#include "kotek_std_alias_hybrid_unordered_set.h"
+#include "kotek_std_alias_static_unordered_set.h"
+
+
+#if defined(KOTEK_USE_LIBRARY_TYPE_EMB) && \
+	defined(KOTEK_USE_STD_LIBRARY_STATIC_CONTAINERS)
+	#define ktk_unordered_set \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
+	#define KTK_UNORDERED_SET \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
+	#define ktkUnorderedSet \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
+#else
+	#define ktk_unordered_set \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
+	#define KTK_UNORDERED_SET \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
+	#define ktkUnorderedSet \
+		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
+#endif
+
+#ifdef KOTEK_USE_LIBRARY_TYPE_EMB
+static_assert(std::is_same_v<kun_kotek kun_ktk static_unordered_set<bool, 1>,
+				  ktk_unordered_set<bool, 1>>,
+	"If library type was defined as EMB (aka static) it means that all "
+	"containers that "
+	"kotek framework provides to use ARE with static_ prefix it means that "
+	"this unordered_set "
+	"container is expected as static_unordered_set implementation");
+#elif defined(KOTEK_USE_LIBRARY_TYPE_DYN)
+static_assert(std::is_same_v<kun_kotek kun_ktk unordered_set<bool, 1>,
+				  ktk_unordered_set<bool, 1>>,
+	"If library type was defined as DYN (aka dynamic) it means that all "
+	"containers that "
+	"kotek framework provides to use ARE WITHOUT static_ prefix and hybrid_ "
+	"prefix it means that "
+	"this unordered_set "
+	"container is expected as unordered_set implementation");
+#elif defined(KOTEK_USE_LIBRARY_TYPE_HYB)
+static_assert(std::is_same_v<kun_kotek kun_ktk hybrid_unordered_set<bool, 1>,
+				  ktk_unordered_set<bool, 1>>,
+	"If library type was defined as HYB (aka hybrid) it means that all "
+	"containers that "
+	"kotek framework provides to use ARE with hybrid_ prefix it means that "
+	"this unordered_set "
+	"container is expected as hybrid_unordered_set implementation");
+#else
+	#error unknown configuration, kotek supports three configurations: DYN (dynamic), EMB (static), HYB (hybrid)
+#endif
 
 KOTEK_BEGIN_NAMESPACE_KOTEK
 KOTEK_BEGIN_NAMESPACE_CORE
--- src/kotek.core.containers.unordered_set/include/kotek_std_alias_unordered_set.h
+++ src/kotek.core.containers.unordered_set/include/kotek_std_alias_unordered_set.h
@@ -11,10 +11,6 @@
 #else
 #endif
 
-#ifdef KOTEK_USE_STD_LIBRARY_STATIC_CONTAINERS
-	#include <etl/unordered_set.h>
-#endif
-
 KOTEK_BEGIN_NAMESPACE_KOTEK
 KOTEK_BEGIN_NAMESPACE_KTK
 
@@ -31,11 +27,6 @@ using unordered_set = std::unordered_set<Key, Hash, Predicate, Allocator>;
 #else
 #endif
 
-#ifdef KOTEK_USE_STD_LIBRARY_STATIC_CONTAINERS
-template <class Key, size_t Size>
-using static_unordered_set = etl::unordered_set<Key, Size>;
-#endif
-
 KOTEK_END_NAMESPACE_KTK
 
 template <class Key, size_t NotInUseArgument = 0,
@@ -45,54 +36,4 @@ template <class Key, size_t NotInUseArgument = 0,
 using unordered_set_t = KUN_KOTEK KUN_KTK
 	unordered_set<Key, NotInUseArgument, Hash, Predicate, Allocator>;
 
-template <class Key, size_t Size>
-using static_unordered_set_t =
-	KUN_KOTEK KUN_KTK static_unordered_set<Key, Size>;
-
 KOTEK_END_NAMESPACE_KOTEK
-
-#if defined(KOTEK_USE_LIBRARY_TYPE_EMB) && \
-	defined(KOTEK_USE_STD_LIBRARY_STATIC_CONTAINERS)
-	#define ktk_unordered_set \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
-	#define KTK_UNORDERED_SET \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
-	#define ktkUnorderedSet \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK static_unordered_set
-#else
-	#define ktk_unordered_set \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
-	#define KTK_UNORDERED_SET \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
-	#define ktkUnorderedSet \
-		KOTEK_USE_NAMESPACE_KOTEK KOTEK_USE_NAMESPACE_KTK unordered_set
-#endif
-
-#ifdef KOTEK_USE_LIBRARY_TYPE_EMB
-static_assert(std::is_same_v<kun_kotek kun_ktk static_unordered_set<bool, 1>,
-				  ktk_unordered_set<bool, 1>>,
-	"If library type was defined as EMB (aka static) it means that all "
-	"containers that "
-	"kotek framework provides to use ARE with static_ prefix it means that "
-	"this unordered_set "
-	"container is expected as static_unordered_set implementation");
-#elif defined(KOTEK_USE_LIBRARY_TYPE_DYN)
-static_assert(std::is_same_v<kun_kotek kun_ktk unordered_set<bool, 1>,
-				  ktk_unordered_set<bool, 1>>,
-	"If library type was defined as DYN (aka dynamic) it means that all "
-	"containers that "
-	"kotek framework provides to use ARE WITHOUT static_ prefix and hybrid_ "
-	"prefix it means that "
-	"this unordered_set "
-	"container is expected as unordered_set implementation");
-#elif defined(KOTEK_USE_LIBRARY_TYPE_HYB)
-static_assert(std::is_same_v<kun_kotek kun_ktk hybrid_unordered_set<bool, 1>,
-				  ktk_unordered_set<bool, 1>>,
-	"If library type was defined as HYB (aka hybrid) it means that all "
-	"containers that "
-	"kotek framework provides to use ARE with hybrid_ prefix it means that "
-	"this unordered_set "
-	"container is expected as hybrid_unordered_set implementation");
-#else
-	#error unknown configuration, kotek supports three configurations: DYN (dynamic), EMB (static), HYB (hybrid)
-#endif
