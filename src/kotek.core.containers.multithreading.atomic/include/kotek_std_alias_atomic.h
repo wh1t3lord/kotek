1a487e97a48a878089eb35eb437e0bf2cf13ad64
wh1t3lord <git.matvey@yandex.ru>
2025-11-07 23:18:13 +0300

    ADD: atomic_flag;

--- src/kotek.core.containers.multithreading.atomic/include/kotek_std_alias_atomic.h
+++ src/kotek.core.containers.multithreading.atomic/include/kotek_std_alias_atomic.h
@@ -14,6 +14,8 @@ KOTEK_BEGIN_NAMESPACE_MT
 #ifdef KOTEK_USE_NOT_CUSTOM_LIBRARY
 template <typename Type>
 using atomic = std::atomic<Type>;
+
+using atomic_flag = std::atomic_flag;
 #else
 #endif
 
@@ -23,6 +25,8 @@ KOTEK_END_NAMESPACE_KTK
 KOTEK_BEGIN_NAMESPACE_MT
 template <typename Type>
 using atomic_t = KUN_KOTEK KUN_KTK KUN_MT atomic<Type>;
+
+using atomic_flag_t = KUN_KOTEK KUN_KTK KUN_MT atomic_flag;
 KOTEK_END_NAMESPACE_MT
 
 KOTEK_END_NAMESPACE_KOTEK
